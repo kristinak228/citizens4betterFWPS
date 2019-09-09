b83a616056fc58dd42727c0561b5db8e8406992b
kristina <kolibak@clarkson.edu>
2019-09-09 18:03:10 -0400

    Semi completed feedback form

--- action.php
+++ action.php
@@ -2,9 +2,9 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: TangledDemo'; 
-    $to = 'demo@tangledindesign.com'; 
-    $subject = 'Hello';
+    $from = $email; 
+    $to = '4betterschools@comcast.net'; 
+    $subject = 'FWPS - New Email From Reader';
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 ?>
